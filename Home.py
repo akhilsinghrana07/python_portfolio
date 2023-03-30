bc246ec6c14e81a65cf8778a07a07fc2f9e14e66
akhilsinghrana07 <akhilsinghrana7@gmail.com>
2023-03-30 03:24:58 -0400

    Resume.py added

--- Home.py
+++ Home.py
@@ -3,35 +3,35 @@ import pandas as pd
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
-
-with col1:
-    st.image('images/photo.png')
-
-with col2:
-    st.title("Akhil Singh Rana")
-    content = """
-    Hi there! I am Akhil Singh Rana, a software engineer with experience in data engineering and web application development. I am currently pursuing Software Engineering Technology at Centennial College in Toronto.
-
-Previously, I completed my Bachelor's in Computer Science and Engineering from SRM Institute of Science and Technology in Chennai, India. During my academic journey, I gained a strong foundation in Python programming, data analysis, and SQL proficiency.
-
-SKILLS:
-
-Python (Pandas, NumPy, Matplotlib, Scikit-learn)
-.NET framework(C#)
-ReactJS( MVC , graphql, RESTFull API)
-PowerBI, Azure (Data lakes, Data Factory, Azure SQL Database, Cosmos DB)
-Microsoft Excel (Macros, V-Lookup, Power Query)
-SQL proficiency, database management, SQL queries
-Git, GitHub (cloning, merging, collaboration, Git actions)
-Web scraping and API integration with Python
-Data cleaning, wrangling, and visualization
-Machine learning (analysis, prediction)
-Docker (containerization, build image, deployment)
-Communication, presentation, and time management
-
-I am passionate about using my technical skills to solve complex problems and improve overall project efficiency and effectiveness. I am always eager to learn and grow, and I am excited about new opportunities in the field of software engineering.
-    """
-    st.info(content)
+with st.container():
+    with col1:
+        st.image('images/photo.png')
+        st.title(f"[LinkedIn]({'https://www.linkedin.com/in/akhilsinghrana729/'})")
+    with col2:
+        st.title("Akhil Singh Rana")
+        content = """
+        Hi there! I am Akhil Singh Rana, a software engineer with experience in data engineering and web application development. I am currently pursuing Software Engineering Technology at Centennial College in Toronto.
+    
+    Previously, I completed my Bachelor's in Computer Science and Engineering from SRM Institute of Science and Technology in Chennai, India. During my academic journey, I gained a strong foundation in Python programming, data analysis, and SQL proficiency.
+    
+    SKILLS:
+    
+    Python (Pandas, NumPy, Matplotlib, Scikit-learn)
+    .NET framework(C#)
+    ReactJS( MVC , graphql, RESTFull API)
+    PowerBI, Azure (Data lakes, Data Factory, Azure SQL Database, Cosmos DB)
+    Microsoft Excel (Macros, V-Lookup, Power Query)
+    SQL proficiency, database management, SQL queries
+    Git, GitHub (cloning, merging, collaboration, Git actions)
+    Web scraping and API integration with Python
+    Data cleaning, wrangling, and visualization
+    Machine learning (analysis, prediction)
+    Docker (containerization, build image, deployment)
+    Communication, presentation, and time management
+    
+    I am passionate about using my technical skills to solve complex problems and improve overall project efficiency and effectiveness. I am always eager to learn and grow, and I am excited about new opportunities in the field of software engineering.
+        """
+        st.info(content)
 
 content2 = """
 Below you can find some of the projects I have built. Feel free to contact me!
---
+++ pages/Resume.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import base64
+import io
+
+st.set_page_config(layout="wide")
+# Set the title of the app
+
+
+# Open the PDF file
+with open("images/Akhil_Singh_Rana_Resume.pdf", "rb") as f:
+    pdf_bytes = f.read()
+
+# Create a column layout
+col1, col2 = st.columns([1, 0.1])
+
+# Display the title in the first column
+with col1:
+    st.title("Resume")
+
+# Display the download button in the second column
+with col2:
+    download_button = st.download_button(
+        label="Download", data=pdf_bytes, file_name="Akhil_Singh_Rana_Resume.pdf"
+    )
+
+# Show the PDF file
+st.markdown(
+    f'<embed src="data:application/pdf;base64,{base64.b64encode(pdf_bytes).decode()}" width="100%" height="600"></embed>',
+    unsafe_allow_html=True,
+)
