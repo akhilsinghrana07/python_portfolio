9bbea251fefa45545b1df7ddbda3e9a69ff5278b
akhilsinghrana07 <akhilsinghrana7@gmail.com>
2023-03-31 03:52:27 -0400

    added background

--- Home.py
+++ Home.py
@@ -1,15 +1,35 @@
 import streamlit as st
 import pandas as pd
+import base64
+
 st.set_page_config(layout="wide")
+
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+        f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+        unsafe_allow_html=True
+    )
+
+
+add_bg_from_local('images/b7.jpg')
 st.title("Akhil Singh Rana")
+
 col1, col2 = st.columns(2)
 with st.container():
     with col1:
-
         st.image('images/photo.png')
         st.title(f"[LinkedIn]({'https://www.linkedin.com/in/akhilsinghrana729/'})")
     with col2:
-
         content = """
         Hi there! I am Akhil Singh Rana, a software engineer with experience in data engineering and web application development. I am currently pursuing Software Engineering Technology at Centennial College in Toronto.
     
@@ -39,7 +59,7 @@ Below you can find some of the projects I have built. Feel free to contact me!
 """
 
 st.write(content2)
-col3, empty_col ,col4 = st.columns([1.5, 0.5, 1.5])
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv("data.csv", sep=";")
 with col3:
@@ -47,11 +67,11 @@ with col3:
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-        #st.write(f"[Source Code]({row['url']})")
+        # st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-        #st.write(f"[Source Code]({row['url']})")
+        # st.write(f"[Source Code]({row['url']})")
